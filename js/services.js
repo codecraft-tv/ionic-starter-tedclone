1c7f2ace8272a4c6dbe500c485936513078f1d49
Asim Hussain <jawache@gmail.com>
2015-12-06 20:41:54 +0000

    Updated to use superfeeder API instead of google feed API which is not depricated (cherry picked from commit ff91da3)

--- js/services.js
+++ js/services.js
@@ -2,41 +2,29 @@ var app = angular.module('tedrssapp.services', []);
 
 app.constant("FEED_URL", "http://feeds.feedburner.com/TEDTalks_video");
 
-app.factory('FeedService', function ($http, $q, FEED_URL) {
+app.factory('FeedService', function ($http, $q, $rootScope, FEED_URL) {
 
 	var self = {
 		'posts': []
 	};
 
-	var parseEntry = function (entry) {
-		var media = (entry && entry.mediaGroups) ? entry.mediaGroups[0].contents[0] : {url: ''};
-		if (media.type == "video/mp4") {
-			entry.thumbnail = media.thumbnails[0].url;
-			entry.video = media.url;
-		} else {
-			entry.thumbnail = media.url;
-		}
-		entry.publishedDate = new Date(entry.publishedDate);
-	};
-
-
 	self.loadFeed = function () {
 
 		self.posts.length = 0;
 		var defer = $q.defer();
-		var params = {"v": "1.0", "callback": "JSON_CALLBACK", "num": 100, "q": FEED_URL};
 
-		$http.jsonp("http://ajax.googleapis.com/ajax/services/feed/load", {params: params})
-			.success(function (res) {
-				console.debug(res);
-
-				angular.forEach(res.responseData.feed.entries, function (entry) {
-					parseEntry(entry);
-					self.posts.push(entry);
-				});
-				defer.resolve(self.posts);
-
+		superfeedr.auth('jawache', '6fcbda900c18d99b6189eeb77ccf063a');
+		superfeedr.setOnLoadCallback(function() {
+			var feed = new superfeedr.Feed(FEED_URL);
+			feed.load(function(result) {
+				$rootScope.$apply(function() {
+					angular.forEach(result.feed.entries, function (entry) {
+						self.posts.push(entry);
+					});
+					defer.resolve(self.posts);
+				});				
 			});
+		});		
 
 		return defer.promise;
 	};
